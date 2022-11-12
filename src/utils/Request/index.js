af720ec6db9adae3c20761cb0ea7150b4ea922cf
miguelortis <pedroarguelles1996@gmail.com>
2022-11-12 11:40:40 -0400

    fix

--- src/utils/Request/index.js
+++ src/utils/Request/index.js
@@ -1,72 +1,61 @@
-import Axios from 'axios'
+import Axios from "axios";
 //process.env.REACT_APP_API_URL
 //process.env.REACT_APP_TEST_URL
-const API_URL = process.env.REACT_APP_TEST_URL
+const API_URL = process.env.REACT_APP_TEST_URL;
 const headers = {
-    authorization: `Bearer ${localStorage.getItem('token')}`,
-}
+  authorization: `Bearer ${localStorage.getItem("token")}`,
+};
 export default {
   get: async (url, params = {}, options = {}) => {
     const response = await Axios.get(`${API_URL}${url}`, {
       headers,
       params,
       validateStatus: function (status) {
-        return status < 500 // Resolve only if the status code is less than 500
+        return status < 500; // Resolve only if the status code is less than 500
       },
       ...options,
-    })
-    return response
+    });
+    return response;
   },
   post: async (url, payload, options = {}) => {
     const headers =
-      localStorage.getItem('authorization') &&
-      localStorage.getItem('authorization') !== 'null'
-        ? { authorization: localStorage.getItem('authorization') }
-        : {}
-    const response = await Axios.post(
-      `${API_URL}${url}`,
-      payload,
-      {
-        headers,
-        validateStatus: function (status) {
-          return status < 500 // Resolve only if the status code is less than 500
-        },
-        ...options,
-      }
-    )
+      localStorage.getItem("authorization") &&
+      localStorage.getItem("authorization") !== "null"
+        ? { authorization: localStorage.getItem("authorization") }
+        : {};
+    const response = await Axios.post(`${API_URL}${url}`, payload, {
+      headers,
+      validateStatus: function (status) {
+        return status < 500; // Resolve only if the status code is less than 500
+      },
+      ...options,
+    });
     //console.log(process.env.REACT_APP_API_URL);
-    return response
+    return response;
   },
   put: async (url, payload, options = {}) => {
-    const response = await Axios.put(
-      `${API_URL}${url}`,
-      payload,
-      {
-        headers,
-        validateStatus: function (status) {
-          return status < 500 // Resolve only if the status code is less than 500
-        },
-      }
-    )
-    return response
+    const response = await Axios.put(`${API_URL}${url}`, payload, {
+      headers,
+      validateStatus: function (status) {
+        return status < 500; // Resolve only if the status code is less than 500
+      },
+    });
+    return response;
   },
   delete: async (url, params = {}, options = {}) => {
     const headers =
-      localStorage.getItem('authorization') &&
-      localStorage.getItem('authorization') !== 'null'
-        ? { authorization: localStorage.getItem('authorization') }
-        : {}
-    const response = await Axios.delete(
-      `${API_URL}${url}`,
-      {
-        headers,
-        params,
-        validateStatus: function (status) {
-          return status < 500 // Resolve only if the status code is less than 500
-        },
-        ...options,
-      }
-    )
-    return response
+      localStorage.getItem("authorization") &&
+      localStorage.getItem("authorization") !== "null"
+        ? { authorization: localStorage.getItem("authorization") }
+        : {};
+    const response = await Axios.delete(`${API_URL}${url}`, {
+      headers,
+      params,
+      validateStatus: function (status) {
+        return status < 500; // Resolve only if the status code is less than 500
+      },
+      ...options,
+    });
+    return response;
   },
-}
+};
